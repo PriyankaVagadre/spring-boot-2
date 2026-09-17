5909c98850a9f6be66097efc4d0a55f3007c0521
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-17 22:19:49 +0530

    project setup

---
+++ employee-m/src/main/java/em_backend/example/employee_m/EmployeeMApplication.java
@@ -0,0 +1,13 @@
+package em_backend.example.employee_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMApplication.class, args);
+	}
+
+}
---
+++ employee-m/src/test/java/em_backend/example/employee_m/EmployeeMApplicationTests.java
@@ -0,0 +1,13 @@
+package em_backend.example.employee_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

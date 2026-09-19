186f0e66a42f20da3fa9ad3b92adb0a5b4ce44ce
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 17:50:31 +0530

    call the getall service method to employeelist component

--- react-app/employee-m-react-app/src/components/EmployeeListComponent.jsx
+++ react-app/employee-m-react-app/src/components/EmployeeListComponent.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
+import { getEmployees } from '../services/EmployeeService';
 
 function EmployeesList() {
-    const [employees, setEmployees] = useState([
-        { id: 1, firstName: 'Employee 1', lastName: 'Last Name 1', email: 'xyz@gmail.com' },
-        { id: 2, firstName: 'Employee 2', lastName: 'Last Name 2', email: 'abc@gmail.com' },
-        { id: 3, firstName: 'Employee 3', lastName: 'Last Name 3', email: 'pqr@gmail.com' },
-    ]);
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+       getEmployees().then(response => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error('Error fetching employees:', error);
+        });
+    }, []);
     return(
         <div>
             <h1 className="text-left">Employee Component</h1>

9a610746d75e2c7fd5ea116a82de62bb1566173a
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 17:36:48 +0530

    iterate through employee list

--- react-app/employee-m-react-app/src/components/EmployeeListComponent.jsx
+++ react-app/employee-m-react-app/src/components/EmployeeListComponent.jsx
@@ -13,28 +13,16 @@ function EmployeesList() {
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Employee 1</h5>
-                                <p className="card-text">This is employee 1.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Employee 2</h5>
-                                <p className="card-text">This is employee 2.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Employee 3</h5>
-                                <p className="card-text">This is employee 3.</p>
-                            </div>
-                        </div>
+                        {
+                            employees.map(employee => (
+                                <div className="card" key={employee.id}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{employee.firstName} {employee.lastName}</h5>
+                                        <p className="card-text">{employee.email}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
@@ -42,3 +30,5 @@ function EmployeesList() {
     );
 
 }
+
+export default EmployeesList;

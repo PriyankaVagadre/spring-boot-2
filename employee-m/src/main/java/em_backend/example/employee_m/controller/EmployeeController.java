38ee04c4240990881d154bb1be3ab5fba4ef222a
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 13:10:35 +0530

    create controller - with post employee api

---
+++ employee-m/src/main/java/em_backend/example/employee_m/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package em_backend.example.employee_m.controller;
+
+import em_backend.example.employee_m.dto.EmployeeDto;
+import em_backend.example.employee_m.services.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    //Build add Employee Api
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto saveEmployee =  employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(saveEmployee,HttpStatus.CREATED);
+    }
+
+}

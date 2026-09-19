bd5f8751c853bdbf179ac4d3420471eba4f8507a
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 19:17:35 +0530

    create add employee form

--- react-app/employee-m-react-app/src/components/EmployeeComponent.jsx
+++ react-app/employee-m-react-app/src/components/EmployeeComponent.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function EmployeeComponent() {
   return (
+    <>
     <div>EmployeeComponent</div>
+    <div className="container">
+        <h1 className="text-center">Add Employee</h1>
+        <form>
+            <div className="form-group">
+                <label className='text-left'>First Name</label>
+                <input type="text" className="form-control" placeholder="Enter first name" />
+            </div>
+            <div className="form-group">
+                <label>Last Name</label>
+                <input type="text" className="form-control" placeholder="Enter last name" />
+            </div>
+            <div className="form-group">
+                <label>Email address</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    </div>  
+    </>
   )
 }
 

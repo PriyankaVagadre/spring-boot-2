cd7dadec3f831bc472ba96d9f7d51314ccfbecfc
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 12:43:38 +0530

    create Mapper class - to connect EmployeeDto to Employee entity and Employee entity to EmployeeDto

---
+++ employee-m/src/main/java/em_backend/example/employee_m/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package em_backend.example.employee_m.mapper;
+
+import em_backend.example.employee_m.dto.EmployeeDto;
+import em_backend.example.employee_m.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}

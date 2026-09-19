2cb974db3cd1f9c0b8c7838f66d8eebbc0ff14b2
priyankavagadre <priyankavagadre@hotmail.com>
2026-09-19 18:13:26 +0530

    Router implemented

--- react-app/employee-m-react-app/src/App.jsx
+++ react-app/employee-m-react-app/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 // import './App.css'
-import EmployeesList from './components/EmployeeListComponent'
+import EmployeesList from './components/EmployeeListComponent';
+import { BrowserRouter, Route, Router, Routes ,Link} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <EmployeesList/>
+    <BrowserRouter>
+    <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<EmployeesList />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
